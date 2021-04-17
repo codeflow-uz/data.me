f0049bdc70818b3a93a019c9e1e08b09b510a83a
zokir6466 <zokirmahamadjonov29@gmail.com>
2021-04-17 17:04:41 +0500

    new admin qo`wiw tayyor

--- routes/newAdmin.js
+++ routes/newAdmin.js
@@ -1,8 +1,41 @@
 const express = require('express');
-const router  = express.Router();
+const {
+  query
+} = require('../public/pool');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('addAdmin')
+  res.render('addAdmin', {
+    msg: ''
+  })
+})
+
+router.post('/', async (req, res) => {
+  let {
+    fullname,
+    login,
+    password,
+    type
+  } = req.body
+
+  let is_excite = await query('select * from admins where login = $1', login)
+
+  if (is_excite.length == 0) {
+    await query('insert into admins ( fullname, login, password, level ) values ($1, $2, $3, $4)',
+      fullname,
+      login,
+      password,
+      type
+    )
+    res.render('addAdmin', {
+      msg: ''
+    })
+  } else {
+    res.render('addAdmin', {
+      msg: 'Bunday login bilan ro`yxatdan o`tilgan.'
+    })
+  }
+
 })
 
 module.exports = router
